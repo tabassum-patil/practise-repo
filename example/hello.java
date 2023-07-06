c87340e9002247168cf45b8bcefa9c9445392e0e
Tabassum <Tabassum.Patil@ibm.com>
2023-07-07 00:12:34 +0530

    ✨ feat: Add method to compute sum in HelloWorld class
    - Added a new method `add` to the `HelloWorld` class in the `hello.java` file.
    - The `add` method takes two integer parameters and prints their sum.
    - This enhancement allows for more functionality in the `HelloWorld` class.
    
    Note: The commit message assumes that the code change is considered a feature addition. If it is a bug fix or other type of change, please let me know so I can adjust the commit message accordingly.

--- example/hello.java
+++ example/hello.java
@@ -2,6 +2,10 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+        add(2,3); 
+    }
+    public void add(int a, int b){
+        System.out.println("Sum:"+(2+3));
     }
 }
